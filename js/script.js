4cdf1afe4dd2ef095243295b4c1d5d7ed930407f
Maryam-Ashfaq10 <maryammashfaq000@gmail.com>
2024-09-21 17:53:29 +0500

    updated js

--- js/script.js
+++ js/script.js
@@ -5,4 +5,38 @@ $(document).ready(function() {
         $(this).hide();
         $('#options').show();
     });
+
+    $('.option-btn').on('click', function() {
+        const option = $(this).data('option');
+
+        switch (option) {
+            case 1:
+                // Handle Random Number
+                const randomNumber = Math.floor(Math.random() * 100) + 1; // Generates a number between 1 and 100
+                alert(randomNumber); // Display the number
+                break;
+            case 2:
+                // Handle Random Name
+                console.log('Random Name selected');
+                break;
+            case 3:
+                // Handle Random Quote
+                console.log('Random Quote selected');
+                break;
+            case 4:
+                // Handle Random Color
+                console.log('Random Color selected');
+                break;
+            case 5:
+                // Handle Random Facts
+                console.log('Random Facts selected');
+                break;
+            case 6:
+                // Handle Random Places
+                console.log('Random Places selected');
+                break;
+            default:
+                console.log('Unknown option selected');
+        }
+    });
 });
